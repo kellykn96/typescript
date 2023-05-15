0b6c28355dd26a183beace6b5462235ef932ea45
Kelly Knoblauch <kellyKnobla@gmail.com>
2023-05-15 14:27:45 -0300

    aula 11 - terminar

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,30 @@
+import { Negociacao } from "../models/negociacao";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+
+    constructor(){
+        this.inputData = document.querySelector('#data') //# é o antigo _ de js;
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+
+    }
+
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao)
+
+    }
+
+    criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+
+
+    }
+}
